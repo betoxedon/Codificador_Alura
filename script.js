8b70cc66e345d26a0b9a53ee0b5c29d36d1dee6d
betoxedon <betoxedon@hotmail.com>
2022-11-20 22:23:54 -0300

    Encriptador concluido

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-const tx = document.getElementsByTagName("textarea");
-for (let i = 0; i < tx.length; i++) {
-  tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
-  tx[i].addEventListener("input", OnInput, false);
+refs = ['e', 'i', 'a', 'o', 'u']
+troca = ['enter', 'imes', 'ai', 'ober', 'ufat']
+
+
+function cripto() {
+  let texto = document.getElementById("entrada").value 
+  for (let i = 0; i<refs.length; i++){
+    texto = texto.replaceAll(refs[i], troca[i])
+  }
+  organize();
+  return document.getElementById("msg-saida").textContent = texto
 }
 
-function OnInput() {
-  this.style.height = "auto";
-  this.style.height = (this.scrollHeight) + "px";
+
+function uncripto() {
+  let texto = document.getElementById("entrada").value 
+  for (let i = 0; i<refs.length; i++){
+    texto = texto.replaceAll(troca[i], refs[i])
+  }
+  organize();
+  return document.getElementById("msg-saida").textContent = texto
 }
+
+async function clipboardCopy() {
+  let tx = document.getElementById("msg-saida").textContent;
+  await navigator.clipboard.writeText(tx);
+}
+
+function organize(){
+  document.getElementById('msg-saida').style.textAlign='justify'
+  document.getElementById("saida").style.display="none"
+  document.getElementById("img-saida").style.display="none"
+  document.getElementById("copy").style.visibility="visible"
+}
+
+
